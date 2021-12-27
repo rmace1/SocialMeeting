9f01407db03786908e8cd100a7553536d3ca018a
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2021-12-26 19:58:56 -0500

    made changes

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,6 +1,8 @@
 package models;
 
 public class Post {
+    @Id// defines this variable as the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer Id;
     String message;
     Integer authorId;  //Foreign Key to User table
